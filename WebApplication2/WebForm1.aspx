ff9f8772a35e15f9bef3822998acf028fe5fbf7c
raziber <razi.berg@gmail.com>
2016-11-15 23:57:55 +0200

    sd
    s

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form name="form1" runat="server" action="WebForm1.aspx" method="post">
+    <div>
+        <input type="text" name="username" placeholder="username" /> <br/>
+        <input type="password" name="password" placeholder="password" /> <br/>
+        <input type="submit" />
+    </div>
+    </form>
+    <%= passFail %>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public string username = "raziber";
+        public string password = "raziberg";
+        public string passFail;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(Request.HttpMethod == "POST")
+            {
+                if(Request["username"] == username && Request["password"] == password)
+                {
+                    passFail = "pass";
+                }
+                else
+                {
+                    passFail = "fail";
+                }
+            }
+        }
+    }
+}
